886d6dc25646d7c533ba8547fae014fe0f2e6da1
calebtripp <calebtripp@outlook.com>
2023-12-04 13:24:51 -0500

    xc1

--- src/exercise/03.js
+++ src/exercise/03.js
@@ -8,12 +8,20 @@ const CountContext = React.createContext()
 function CountProvider(props) {
   const [count, setCount] = React.useState(0)
   const value = [count, setCount]
-
   return <CountContext.Provider value={value} {...props} />
 }
 
+function useCount() {
+  const context = React.useContext(CountContext)
+  if (!context) {
+    throw new Error('useCount must be used within a CountProvider')
+  }
+
+  return context
+}
+
 function CountDisplay() {
-  const [count] = React.useContext(CountContext)
+  const [count] = useCount()
   return <div>{`The current count is ${count}`}</div>
 }
 
